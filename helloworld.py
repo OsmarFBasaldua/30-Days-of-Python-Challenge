08e45afe9e1795a25edee90ef6e0116fe924d8b2
Osmar Feregrino <90415074+OsmarFBasaldua@users.noreply.github.com>
2026-02-09 17:03:23 -0600

    add my first python File

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+# Day 1 - 30 Days of Python Challenge
+
+print("Hello, World!") # Printing a string - My first Python code!
+
+print(2+3) # Addition (+)
+print(3-1) # Subtraction (-)
+print(2*3) # Multiplication (*)
+print(3/2) # Division (/)
+print(3 ** 2) # Exponentiation (**)
+print(3 % 2) # Modulus (%)
+print(3 // 2) # Floor division operator (//)
+
+# Checking data types
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1 + 2j)) # complex number
+print(type('Osmar')) # string
+print(type([1, 2, 3])) # list
+print(type({'name': 'Osmar', 'age': 30})) # dictionary
+print(type({9.8, 3.14, 2.7})) # set
+print(type((9.8, 3.14, 2.7))) # tuple
